8b83f3ae8c3e068ef4ffb835fc16d98180a3f2ec
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2025-06-08 22:46:46 +0530

    Update leavevc.py

--- SHUKLA/plugins/vcbot/leavevc.py
+++ SHUKLA/plugins/vcbot/leavevc.py
@@ -1,6 +1,5 @@
 from asyncio.queues import QueueEmpty
 from pyrogram import filters
-from pytgcalls.exceptions import GroupCallNotFound
 
 from ... import *
 from ...modules.mongo.streams import *
@@ -13,20 +12,19 @@ async def leave_vc(client, message):
     chat_id = message.chat.id
     try:
         a = await call.get_call(chat_id)
-        if (a.status == "not_playing"
+        if (
+            a.status == "not_playing"
             or a.status == "playing"
             or a.status == "paused"
-        ):  
+        ):
             try:
                 queues.clear(chat_id)
             except QueueEmpty:
                 pass
             await call.leave_group_call(chat_id)
             await eor(message, "**Left VC!**")
-    except GroupCallNotFound:
+    except Exception:
         await eor(message, "**I am Not in VC!**")
-    except Exception as e:
-        print(f"Error: {e}")
 
 
 @app.on_message(cdz(["clve", "cleave", "cleavevc"]))
@@ -35,22 +33,19 @@ async def leave_vc_(client, message):
     user_id = message.from_user.id
     chat_id = await get_chat_id(user_id)
     if chat_id == 0:
-        return await eor(message,
-            "**🥀 No Stream Chat Set❗**"
-    )
+        return await eor(message, "**🥀 No Stream Chat Set❗**")
     try:
         a = await call.get_call(chat_id)
-        if (a.status == "not_playing"
+        if (
+            a.status == "not_playing"
             or a.status == "playing"
             or a.status == "paused"
-        ):  
+        ):
             try:
                 queues.clear(chat_id)
             except QueueEmpty:
                 pass
             await call.leave_group_call(chat_id)
             await eor(message, "**Left VC!**")
-    except GroupCallNotFound:
+    except Exception:
         await eor(message, "**I am Not in VC!**")
-    except Exception as e:
-        print(f"Error: {e}")
