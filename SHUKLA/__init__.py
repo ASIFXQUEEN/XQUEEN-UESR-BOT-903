f930100a8a03984cbca656ca321b59425a51f96b
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2025-06-08 22:01:16 +0530

    Update __init__.py

--- SHUKLA/__init__.py
+++ SHUKLA/__init__.py
@@ -1,15 +1,17 @@
-from SHUKLA.console import  SUDO_USERS, OWNER_ID, OWNER_USERNAME
+from SHUKLA.console import SUDO_USERS, OWNER_ID, OWNER_USERNAME
 from aiohttp import ClientSession
+import asyncio
 
 # Ai-Userbot Version
 __version__ = "v2.1.0"
 
 spam_chats = []
-aiosession = ClientSession()
+
+aiosession = None  # ClientSession abhi nahi bana rahe, baad me karenge
+
 SUDO_USER = SUDO_USERS
 OWNER_USERNAME = OWNER_USERNAME
 SUDO_USERS.append(OWNER_ID)
-
 
 # All Clients
 from .modules.clients.clients import (
@@ -19,7 +21,6 @@ app = app
 bot = bot
 call = call
 
-
 # Command Handlers
 from .modules.helpers.filters import (
     commandx, commandz
@@ -27,28 +28,23 @@ from .modules.helpers.filters import (
 cdx = commandx
 cdz = commandz
 
-
 # Edit Or Reply
 from .modules.helpers.events import (
     edit_or_reply
 )
 eor = edit_or_reply
 
-
 # Logger
 from .console import LOGGER
 logs = LOGGER
-
 
 # Plugins
 from .console import PLUGINS
 plugs = PLUGINS
 
-
 # Variables
 from . import console as config
 vars = config
-
 
 # Decorators
 from .modules.helpers.wrapper import (
@@ -56,4 +52,3 @@ from .modules.helpers.wrapper import (
 )
 super_user_only = super_user_only
 sudo_users_only = sudo_users_only
-
