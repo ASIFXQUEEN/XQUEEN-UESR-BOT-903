aba285bd90c52a78dbf4a55d02dbfda2027011f7
RAJ RAUTRAY <153398574+ASIFXQUEEN@users.noreply.github.com>
2025-06-08 22:50:31 +0530

    Update pause.py

--- SHUKLA/plugins/vcbot/pause.py
+++ SHUKLA/plugins/vcbot/pause.py
@@ -1,8 +1,6 @@
 from ... import *
 from ...modules.mongo.streams import *
 from pyrogram import filters
-from pytgcalls.exceptions import GroupCallNotFound
-
 
 @app.on_message(cdx(["pse", "pause"]) & ~filters.private)
 @sudo_users_only
@@ -17,11 +15,11 @@ async def pause_stream(client, message):
             await eor(message, "**Already Paused!**")
         elif a.status == "not_playing":
             await eor(message, "**Nothing Streaming!**")
-    except GroupCallNotFound:
-        await eor(message, "**I am Not in VC!**")
     except Exception as e:
-        print(f"Error: {e}")
-
+        if "GroupCallNotFound" in str(e):
+            await eor(message, "**I am Not in VC!**")
+        else:
+            print(f"Error: {e}")
 
 @app.on_message(cdz(["cpse", "cpause"]))
 @sudo_users_only
@@ -29,9 +27,7 @@ async def pause_stream_(client, message):
     user_id = message.from_user.id
     chat_id = await get_chat_id(user_id)
     if chat_id == 0:
-        return await eor(message,
-            "**🥀 No Stream Chat Set❗**"
-    )
+        return await eor(message, "**🥀 No Stream Chat Set❗**")
     try:
         a = await call.get_call(chat_id)
         if a.status == "playing":
@@ -41,9 +37,8 @@ async def pause_stream_(client, message):
             await eor(message, "**Already Paused!**")
         elif a.status == "not_playing":
             await eor(message, "**Nothing Streaming!**")
-    except GroupCallNotFound:
-        await eor(message, "**I am Not in VC!**")
     except Exception as e:
-        print(f"Error: {e}")
-
-  
+        if "GroupCallNotFound" in str(e):
+            await eor(message, "**I am Not in VC!**")
+        else:
+            print(f"Error: {e}")
